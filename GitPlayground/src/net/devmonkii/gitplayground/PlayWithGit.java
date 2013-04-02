0b603799f000eebe839f5e17ae481867c927b908
Daniel Björnlund <dabj001@student.hgo.se>
2013-04-02 19:49:54 +0200

    Adding a new line of code to the branchplay-branch.

--- GitPlayground/src/net/devmonkii/gitplayground/PlayWithGit.java
+++ GitPlayground/src/net/devmonkii/gitplayground/PlayWithGit.java
@@ -6,6 +6,6 @@ public class PlayWithGit
 	{
 		System.out.println("New change");
 		System.out.println("This change should only be reflected in the new branch (branchplay), atleast for now");
+		System.out.println("Adding a new line of code to the branchplay-branch, after merging with master");
 	}
-
 }
