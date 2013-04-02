b0165db57a27a2f14357bdc64fcb2c27758bebf2
Daniel Björnlund <dabj001@student.hgo.se>
2013-04-02 19:06:27 +0200

    Added a new System.out.println message.

--- GitPlayground/src/net/devmonkii/gitplayground/PlayWithGit.java
+++ GitPlayground/src/net/devmonkii/gitplayground/PlayWithGit.java
@@ -5,6 +5,7 @@ public class PlayWithGit
 	public static void main(String[] args)
 	{
 		System.out.println("New change");
+		System.out.println("This change should only be reflected in the new branch (branchplay), atleast for now");
 	}
 
 }
